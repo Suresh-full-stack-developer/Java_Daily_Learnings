3c3264c87d5f87d1b3e25a419e5a4667d8794ffa
Suresh <sureshsureshpp100@gmail.com>
2026-09-14 16:01:01 +0530

    Daily Code

--- Qsp_Java/src/numbers/practice.java
+++ Qsp_Java/src/numbers/practice.java
@@ -155,41 +155,84 @@ public class practice {
 
 		// To find how many notes present in the given amount
 
-		int Amount = 100000;
-
-		System.out.println("To find how many notes present in the given amount:)");
-
-		if (Amount >= 2000) {
-			System.out.println("2000 rupe:" + Amount / 2000);
+//		int Amount = 100000;
+//
+//		System.out.println("To find how many notes present in the given amount:)");
+//
+//		if (Amount >= 2000) {
+//			System.out.println("2000 rupe:" + Amount / 2000);
+//		}
+//		if (Amount >= 500) {
+//			System.out.println("500 rupee:" + Amount / 500);
+//		}
+//		if (Amount >= 200) {
+//			System.out.println("200 rupee:" + Amount / 200);
+//		}
+//		if (Amount >= 100) {
+//			System.out.println("100 rupee:" + Amount / 100);
+//		}
+//		if (Amount >= 50) {
+//			System.out.println("50 rupee:" + Amount / 50);
+//		}
+//		if (Amount >= 20) {
+//			System.out.println("20 rupee:" + Amount / 20);
+//		}
+//		if (Amount >= 10) {
+//			System.out.println("10 rupee:" + Amount / 10);
+//		}
+//
+//		if (Amount >= 5) {
+//			System.out.println("5 rupee coin:" + Amount / 5);
+//		}
+//		if (Amount >= 2) {
+//			System.out.println("2 rupee coin:" + Amount / 2);
+//		}
+//		if (Amount >= 1) {
+//			System.out.println("1 rupee coin:" + Amount / 1);
+//		}
+		
+		
+		
+		
+		// print the natural number
+		
+		
+//		int num=10;
+//		
+//		for(int i=0;i<=num;i++) {
+//			
+//			System.out.println(i);
+//		}
+//		
+//		
+//		//Reverse order
+//		System.out.println("------------------------");
+//		
+//		int numm1=10;
+//		
+//		for(int i=num;i>=0;i--) {
+//			System.out.println(i);
+//		}
+		
+		
+		// Extract the last digit in given number
+		
+//		int num=5238;
+//		while(num>0) {
+//			 num=num/10;
+//			System.out.println(num);
+//		}
+		
+		// sum each digit
+		int num=1234;
+		int sum=0;
+		
+		for(int i=0;i<=num;i++) {
+			
+			num=num%10;
+			sum+=num/10;
+			System.out.println(sum);
+			
 		}
-		if (Amount >= 500) {
-			System.out.println("500 rupee:" + Amount / 500);
-		}
-		if (Amount >= 200) {
-			System.out.println("200 rupee:" + Amount / 200);
-		}
-		if (Amount >= 100) {
-			System.out.println("100 rupee:" + Amount / 100);
-		}
-		if (Amount >= 50) {
-			System.out.println("50 rupee:" + Amount / 50);
-		}
-		if (Amount >= 20) {
-			System.out.println("20 rupee:" + Amount / 20);
-		}
-		if (Amount >= 10) {
-			System.out.println("10 rupee:" + Amount / 10);
-		}
-
-		if (Amount >= 5) {
-			System.out.println("5 rupee coin:" + Amount / 5);
-		}
-		if (Amount >= 2) {
-			System.out.println("2 rupee coin:" + Amount / 2);
-		}
-		if (Amount >= 1) {
-			System.out.println("1 rupee coin:" + Amount / 1);
-		}
-
 	}
 }
