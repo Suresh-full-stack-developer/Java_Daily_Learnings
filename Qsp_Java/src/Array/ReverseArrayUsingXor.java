dfc43024a8b16652cbdfbb88b2950307ba343a04
Suresh <sureshsureshpp100@gmail.com>
2026-09-15 21:02:42 +0530

    Daily Code

---
+++ Qsp_Java/src/Array/ReverseArrayUsingXor.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ReverseArrayUsingXor {
+
+	public static void main(String[] args) {
+		int[] a = {10, 20, 30, 40, 50};
+
+        int i = 0;
+        int j = a.length - 1;
+
+        while (i < j) {
+            a[i] = a[i] ^ a[j];
+            a[j] = a[i] ^ a[j];
+            a[i] = a[i] ^ a[j];
+
+            i++;
+            j--;
+        }
+
+        System.out.println(Arrays.toString(a));
+
+	}
+
+}
