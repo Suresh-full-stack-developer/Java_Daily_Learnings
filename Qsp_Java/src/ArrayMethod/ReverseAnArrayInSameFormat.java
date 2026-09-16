885e956416897cb7eb97d05ef08fd87284aae4ed
Suresh <sureshsureshpp100@gmail.com>
2026-09-16 15:55:24 +0530

    Daily code

---
+++ Qsp_Java/src/ArrayMethod/ReverseAnArrayInSameFormat.java
@@ -0,0 +1,31 @@
+package ArrayMethod;
+
+public class ReverseAnArrayInSameFormat {
+
+	// print like this [10,20,30,40,50,60,70]
+
+	static void revArr(int ar[]) {
+
+		System.out.print("[");
+
+		for (int i = 0; i <= ar.length - 1; i++) {
+
+			System.out.print(ar[i]);
+
+			if (i <= ar.length - 2) {
+				System.out.print(",");
+			}
+
+		}
+
+		System.out.println("]");
+
+	}
+
+	public static void main(String[] args) {
+		int ar[] = { 10, 20, 30, 40, 50, 60, 70 };
+		revArr(ar);
+
+	}
+
+}
