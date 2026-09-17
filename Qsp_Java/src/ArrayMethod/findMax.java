b8fc89eae06f95840ca4038944e24119ff78f227
Suresh <sureshsureshpp100@gmail.com>
2026-09-17 19:10:10 +0530

    Daily Code

---
+++ Qsp_Java/src/ArrayMethod/findMax.java
@@ -0,0 +1,27 @@
+package ArrayMethod;
+
+public class findMax {
+	
+	static int findMax(int ar[]){
+		int firstMax=Integer.MIN_VALUE;
+		for(int i=0;i<=ar.length-1;i++) {
+			if(ar[i]>firstMax) {
+				firstMax=ar[i];
+			}
+		}
+		return firstMax;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		int ar[]= {26,35,46,18,422,520};
+		
+		int res=findMax(ar);
+		
+		System.out.println(res);
+		
+
+	}
+
+}
