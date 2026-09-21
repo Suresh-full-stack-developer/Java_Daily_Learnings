c372e419ff4bbf4748e2be358638cbe05e3b0f25
Suresh <sureshsureshpp100@gmail.com>
2026-09-21 22:56:26 +0530

    Daily Practice

--- Qsp_Java/src/SortingConcepts/BubbleSort.java
+++ Qsp_Java/src/SortingConcepts/BubbleSort.java
@@ -1,9 +1,23 @@
 package SortingConcepts;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		
+
+		int ar[] = { 20, 60, 80, 65, 45, 80, 95 };
+
+		for (int j = 0; j <= ar.length - 1; j++) {
+			for (int i = 0; i <= ar.length - 2; i++) {
+				if (ar[i] > ar[i + 1]) {
+					ar[i] = ar[i] ^ ar[i + 1];
+					ar[i + 1] = ar[i] ^ ar[i + 1];
+					ar[i] = ar[i] ^ ar[i + 1];
+				}
+			}
+		}
+		System.out.println(Arrays.toString(ar));
 
 	}
 
