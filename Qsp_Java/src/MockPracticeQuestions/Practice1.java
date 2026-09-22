3f0d37627c4f6b44d1626f4eea870d8ceb094679
Suresh <sureshsureshpp100@gmail.com>
2026-09-22 22:52:14 +0530

    Daily Practice

--- Qsp_Java/src/MockPracticeQuestions/Practice1.java
+++ Qsp_Java/src/MockPracticeQuestions/Practice1.java
@@ -1,9 +1,24 @@
 package MockPracticeQuestions;
 
+import java.util.Arrays;
+
 public class Practice1 {
 
 	public static void main(String[] args) {
 	
+		
+		int ar[]= {40,50,60,10,35,15,45};
+		
+		for (int j = 0; j <= ar.length - 1; j++) {
+			for (int i = 0; i <= ar.length - 2; i++) {
+				if (ar[i] > ar[i + 1]) {
+					ar[i] = ar[i] ^ ar[i + 1];
+					ar[i + 1] = ar[i] ^ ar[i + 1];
+					ar[i] = ar[i] ^ ar[i + 1];
+				}
+			}
+		}
+		System.out.println(Arrays.toString(ar));
 
 	}
 
